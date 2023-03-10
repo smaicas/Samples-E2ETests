f18dafe3deb716ec30316d8c78fb2b8ad1ce4a9d
smaicas <samuel.maicas.development@gmail.com>
2023-03-10 18:09:51 +0100

    Add MVVMWebSockets

--- ContactForm.Web/Controllers/HomeController.cs
+++ ContactForm.Web/Controllers/HomeController.cs
@@ -1,56 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using WebTestPlaywright.Models;
 
-namespace WebTestPlaywright.Controllers
+namespace WebTestPlaywright.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
+
+    public IActionResult Index() => View();
+
+    public IActionResult Form()
     {
-        private readonly ILogger<HomeController> _logger;
+        HomeFormViewModel model = new();
+        return View(model);
+    }
 
-        public HomeController(ILogger<HomeController> logger)
+    [HttpPost]
+    public IActionResult Form(HomeFormModel Form)
+    {
+        if (Form.BirthDate.HasValue && Form.BirthDate.Value.Date >= DateTime.Today)
         {
-            _logger = logger;
+            ModelState.AddModelError("Form.BirthDate", "The birth date is invalid.");
         }
-
-        public IActionResult Index()
+        if (!ModelState.IsValid)
         {
-            return View();
-        }
-
-        public IActionResult Form()
-        {
-            HomeFormViewModel model = new HomeFormViewModel();
+            HomeFormViewModel model = new()
+            {
+                Form = Form
+            };
             return View(model);
         }
 
-        [HttpPost]
-        public IActionResult Form(HomeFormModel Form)
-        {
-            if(Form.BirthDate.HasValue && Form.BirthDate.Value.Date >= DateTime.Today)
-            {
-                ModelState.AddModelError("Form.BirthDate", "The birth date is invalid.");
-            }
-            if(!ModelState.IsValid)
-            {
-                HomeFormViewModel model = new HomeFormViewModel();
-                model.Form = Form;
-                return View(model);
-            }
-
-            TempData["FullName"] = string.Format("{0} {1}", Form.FirstName, Form.LastName);
-            return RedirectToAction("Success");
-        }
-
-        public IActionResult Success()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        TempData["FullName"] = string.Format("{0} {1}", Form.FirstName, Form.LastName);
+        return RedirectToAction("Success");
     }
+
+    public IActionResult Success() => View();
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
